b48e0b816f7a872ff81533a073c2c188a24c6593
AdityaRaghav0112 <adityaraghav239@gmail.com>
2025-10-13 23:08:50 +0530

    updated projects sections and started skills section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './components/Header'
 import Hero from './components/Hero'
 import Projects from './components/Projects'
+import Skill from './components/Skill';
 
 const App = () => {
 
@@ -11,6 +12,7 @@ const App = () => {
       <Header/>
       <Hero/>
       <Projects/>
+      <Skill/>
     </div>
   )
 }
