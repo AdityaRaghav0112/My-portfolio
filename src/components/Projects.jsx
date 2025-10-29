6b55d976351bee915497abaf0d3e945c20bb9f17
AdityaRaghav0112 <adityaraghav239@gmail.com>
2025-10-29 19:51:15 +0530

    updated resonsiveness

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,30 +3,33 @@ import ThreeDCardDemo from "./ThreeDCardDemo";
 
 const Projects = () => {
   return (
-    <div className="mt-20 flex flex-col items-center p-4 sm:p-6 md:p-8 lg:p-10 gap-5">
+    <div className="mt-10 sm:mt-16 flex flex-col items-center px-4 sm:px-6 md:px-10 gap-4 sm:gap-6">
       {/* Title */}
       <h1 className="text-white text-3xl sm:text-4xl md:text-5xl font-semibold text-center">
         Latest Projects
       </h1>
 
       {/* Subtitle */}
-      <p className="text-center text-base sm:text-lg md:text-xl text-gray-400 font-semibold max-w-2xl">
+      <p className="text-center text-sm sm:text-lg text-gray-400 font-semibold max-w-2xl">
         A glimpse into my recent work and latest explorations.
       </p>
 
       {/* PROJECT SHOWCASE */}
       <div
         className="
-          projects 
-          grid 
-          grid-cols-1 
-          sm:grid-cols-2 
-          lg:grid-cols-3 
-          gap-10 
-          sm:gap-12 
-          md:gap-16 
-          mt-14 
-          w-full 
+          projects
+          grid
+          grid-cols-1
+          sm:grid-cols-2
+          lg:grid-cols-3
+          gap-6
+          sm:gap-10
+          md:gap-14
+          mt-8
+          sm:mt-14
+          mb-10
+          sm:mb-20
+          w-full
           justify-items-center
         "
       >
